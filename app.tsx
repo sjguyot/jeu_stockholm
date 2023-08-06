04fabc5717bee0af0613f3098f182acfcd8ae662
Simon Guyot <courrier@simon-guyot.com>
2023-08-06 19:24:17 +0200

    emoji

--- app.tsx
+++ app.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 
 function App() {
-    return <h1>Bonjour</h1>
+    return <h1>🥞🥞🥞🗻🗻🗻🙈🙉🙊</h1>
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
