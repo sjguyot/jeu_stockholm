2ec598fb14140d8bdff379a7f293d78df5f0ecb4
sjguyot <106873744+sjguyot@users.noreply.github.com>
2023-08-07 13:36:20 +0200

    terminé

--- app.tsx
+++ app.tsx
@@ -7,10 +7,18 @@ function MonBoutton() {
     function handleClick() {
         setCompteur(compteur +1);
     }
+    function handleClick2(){
+        setCompteur(compteur -1)
+    }
+    function handleClick3(){
+        setCompteur(compteur = 0)
+    }
     return(
         <>
         Val = {compteur}
                 <button onClick={handleClick}>+1</button>
+                <button onClick={handleClick2}>-1</button>
+                <button onClick={handleClick3}>reset</button>
         </>
     )
 }
